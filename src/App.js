8175ec0ab52433ee17b9d7291c1fb780720fb5f8
lamnaourm <mohammed.lamnaour@gmail.com>
2022-09-19 16:55:17 +0100

    Correction question 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+var etudiants = [
+  { nom: "Manar", math: 10.5, phys: 12.0, fr: 16.0 },
+  { nom: "Mohamed", math: 13.0, phys: 18.0, fr: 7.0 },
+  { nom: "Khalid", math: 15.0, phys: 15.0, fr: 13.0 },
+  { nom: "Ikbal", math: 2.6, phys: 9.0, fr: 10.0 },
+  { nom: "Aziz", math: 10.0, phys: 11.5, fr: 15.0 },
+  { nom: "Said", math: 12.0, phys: 5.0, fr: 13.0 },
+  { nom: "Moujahid", math: 20.0, phys: 15.0, fr: 18.0 },
+  { nom: "Riyad", math: 18.0, phys: 13.0, fr: 15.0 },
+];
+
+function Liste1() {
+  return <ul>
+      {etudiants.map(item => <li>{item.nom} - Moyenne : {((item.math+item.phys+item.fr)/3).toFixed(2)} </li>)}
+  </ul>
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>
+    <Liste1 />
+  </div>;
 }
 
 export default App;
